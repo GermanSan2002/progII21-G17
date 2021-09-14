e88a91fe8c3b3430ee50a2e7b1fecabdaa66d920
GermanSan2002 <germansan2002@outlook.com>
2021-09-14 13:43:47 -0300

    Creacion de clases Alumno, Profesor, Grupo, Idioma, Lugar, Tipo y PalabraClave.

---
+++ src/lugares/modelos/Lugar.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lugares.modelos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Lugar {
+    String nombre;
+    
+    String ToString(){
+        return "";
+    }
+}
---
+++ src/tipos/modelos/Tipo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipos.modelos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Tipo {
+    String nombre;
+    
+    String ToString(){
+        return "";
+    }
+}
